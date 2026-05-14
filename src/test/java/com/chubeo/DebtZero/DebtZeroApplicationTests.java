57a105b9202759d9ec6fddab28d1f9e0c1eea6b3
TruongVoKy2k5 <chuhonganh0701@gmail.com>
2026-05-14 10:24:11 +0700

    fix: add test properties for ApplicationContext test

--- src/test/java/com/chubeo/DebtZero/DebtZeroApplicationTests.java
+++ src/test/java/com/chubeo/DebtZero/DebtZeroApplicationTests.java
@@ -2,12 +2,20 @@ package com.chubeo.DebtZero;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:postgresql://localhost:5432/debtzero_test",
+        "spring.datasource.username=test",
+        "spring.datasource.password=test",
+        "jwt.secret=404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970",
+        "jwt.expiration=86400000",
+        "jwt.refresh-expiration=604800000",
+        "admin.default-password=admin123"
+})
 class DebtZeroApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
